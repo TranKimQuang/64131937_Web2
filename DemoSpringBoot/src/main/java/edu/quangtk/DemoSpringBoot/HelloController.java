756f33d281de7f7b43b78993c4f24d57b48d9dda
Trần Kim Quang <162008715+TranKimQuang@users.noreply.github.com>
2025-03-04 10:46:56 +0700

    up(2)

--- DemoSpringBoot/src/main/java/edu/quangtk/DemoSpringBoot/HelloController.java
+++ DemoSpringBoot/src/main/java/edu/quangtk/DemoSpringBoot/HelloController.java
@@ -1,11 +1,13 @@
 package edu.quangtk.DemoSpringBoot;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 	
-	@RequestMapping("/helloView")
-	public String hello(){
+	@RequestMapping("/hello")
+	public String hello(ModelMap m){
+		m.addAttribute("tb", "Dữ liệu thông báo xin chào.");
 	return "helloView";
 }
 }
