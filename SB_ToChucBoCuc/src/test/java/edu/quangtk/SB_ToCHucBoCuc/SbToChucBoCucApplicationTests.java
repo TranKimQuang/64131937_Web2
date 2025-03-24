bc9a3a998321324474189779937a079ffc6374d7
Trần Kim Quang <162008715+TranKimQuang@users.noreply.github.com>
2025-03-24 23:36:51 +0700

    tao du an
    tao du an sb luyen tap fragment

---
+++ SB_ToChucBoCuc/src/test/java/edu/quangtk/SB_ToCHucBoCuc/SbToChucBoCucApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.quangtk.SB_ToCHucBoCuc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbToChucBoCucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
