cea4770a26032851624d865ec6eaf4dea20162b7
Trần Kim Quang <162008715+TranKimQuang@users.noreply.github.com>
2025-03-25 09:04:45 +0700

    tạo dự án

---
+++ TongHopGK/src/test/java/edu/quangtk/TongHopGK/TongHopGkApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.quangtk.TongHopGK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TongHopGkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
