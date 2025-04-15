2a6639db9b0be4fc4b9ce73ca3ab36e9bd8b3054
Trần Kim Quang <162008715+TranKimQuang@users.noreply.github.com>
2025-04-15 10:07:21 +0700

    tạo dự án
    push 1

---
+++ TranKimQuang_fitCMS_Blog-1/src/main/java/edu/quangtk/thiGK/ntu64131937/TranKimQuangFitCmsBlog1Application.java
@@ -0,0 +1,13 @@
+package edu.quangtk.thiGK.ntu64131937;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranKimQuangFitCmsBlog1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranKimQuangFitCmsBlog1Application.class, args);
+	}
+
+}
---
+++ TranKimQuang_fitCMS_Blog-1/src/test/java/edu/quangtk/thiGK/ntu64131937/TranKimQuangFitCmsBlog1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.quangtk.thiGK.ntu64131937;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranKimQuangFitCmsBlog1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
