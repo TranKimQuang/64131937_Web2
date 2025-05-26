db53115f35172359db06e4400c3a3fb0b96387b4
Trần Kim Quang <162008715+TranKimQuang@users.noreply.github.com>
2025-05-26 16:53:50 +0700

    tạo dự án

---
+++ QuizApp_64131937/src/main/java/edu/quangtk/QuizApp64131937Application.java
@@ -0,0 +1,13 @@
+package edu.quangtk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizApp64131937Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizApp64131937Application.class, args);
+	}
+
+}
---
+++ QuizApp_64131937/src/test/java/edu/quangtk/QuizApp64131937ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.quangtk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApp64131937ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
