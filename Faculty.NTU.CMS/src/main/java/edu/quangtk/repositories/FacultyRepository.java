2f1695330755e52fac1d5f21d8a7cb250111d862
Trần Kim Quang <162008715+TranKimQuang@users.noreply.github.com>
2025-05-26 19:52:12 +0700

    xoá dự án do xung đột server

--- Faculty.NTU.CMS/src/main/java/edu/quangtk/repositories/FacultyRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.quangtk.repositories;
-
-import edu.quangtk.entity.Faculty;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacultyRepository extends JpaRepository<Faculty, Long> {
-    Faculty findBySlug(String slug);
-}
