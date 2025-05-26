4d094da1d881cc9517875ff2d7d2c3330ceafeb0
Trần Kim Quang <162008715+TranKimQuang@users.noreply.github.com>
2025-05-26 19:55:15 +0700

    thêm chức năng tìm kiếm bằng pageable

--- QuizApp_64131937/src/main/java/edu/quangtk/repository/ExamRepository.java
+++ QuizApp_64131937/src/main/java/edu/quangtk/repository/ExamRepository.java
@@ -1,9 +1,12 @@
 package edu.quangtk.repository;
 
 import edu.quangtk.model.Exam;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExamRepository extends JpaRepository<Exam, Long> {
+  Page<Exam> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 }
