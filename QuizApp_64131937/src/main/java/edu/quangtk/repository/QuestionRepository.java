4cf4275d74fbc443aa6967ccf49a7cec1bb508cd
Trần Kim Quang <162008715+TranKimQuang@users.noreply.github.com>
2025-05-26 19:55:52 +0700

    cập nhật tìm kiếm

--- QuizApp_64131937/src/main/java/edu/quangtk/repository/QuestionRepository.java
+++ QuizApp_64131937/src/main/java/edu/quangtk/repository/QuestionRepository.java
@@ -1,6 +1,8 @@
 package edu.quangtk.repository;
 
 import edu.quangtk.model.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import java.util.List;
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
   List<Question> findByExamId(Long examId);
+  Page<Question> findByExamIdAndContentContainingIgnoreCase(Long examId, String content, Pageable pageable);
 }
